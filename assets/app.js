e937a9b7e49f97dca147fcafa5094afd3e175e54
40sherrin <fareedaskari42@gmail.com>
2021-03-14 20:28:08 +1100

    Working with Date() object

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,30 @@
-const people = [
-  {name: 'karim', age: 20, postion: 'developer', id: 1, salary: 200},
-  {name: 'rahim', age: 20, postion: 'designer', id: 2, salary: 300},
-  {name: 'sherrin', age: 20, postion: 'the boss', id: 3, salary: 240},
-  {name: 'afraz', age: 20, postion: 'developer', id: 4, salary: 260},
-  {name: 'farwa', age: 20, postion: 'manager', id: 5, salary: 400}
+const months = [
+  'January', 
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
 ]
 
-const total = people.reduce(function(acc, currSalary){
-  console.log(`Total ${acc}`);
-  console.log(`Current mony: $${currSalary.salary}`);
-  return acc += currSalary.salary;
-},0)
+const getDay = new Date().getDay();
+const getMonth = new Date().getMonth();
 
-console.log(total);
+console.log(days[getDay])
+console.log(months[getMonth])
