4f7f18155fca401ac05e3aff93548f2eb5ee7c51
40sherrin <fareedaskari42@gmail.com>
2021-03-16 22:48:44 +1100

    code update

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,3 @@
-const h1 = document.createElement('h1');
-const txt = document.createTextNode('Text come from Javascript');
+const heading = document.querySelector('h2');
+const button = document.querySelector('button');
 
-h1.appendChild(txt);
-
-document.body.appendChild(h1);
