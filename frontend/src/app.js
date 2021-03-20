908816c1612b49f5470a1e22e9b219541941c759
40sherrin <fareedaskari42@gmail.com>
2021-03-20 22:06:37 +1100

    redux configuration

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux';
+import {getPosts} from './actions/posts';
 import Posts from './components/posts/posts';
 import Form from './components/form/form';
 import memories from './images/memories.png';
@@ -7,6 +9,10 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles(); 
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
   return (
     <Container maxidth="lg">
         <AppBar className={classes.appBar} position="static" color="inherit">
--- frontend/src/components/posts/posts.js
+++ frontend/src/components/posts/posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Post from './post/post';
 import useStyles from './styles';
 
