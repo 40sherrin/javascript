04361b2b451b83d5f8fea92e8bbe89b0e322b012
40sherrin <fareedaskari42@gmail.com>
2021-03-20 20:43:49 +1100

    Style update

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,13 +3,15 @@ import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import Posts from './components/posts/posts';
 import Form from './components/form/form';
 import memories from './images/memories.png';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles(); 
   return (
     <Container maxidth="lg">
-        <AppBar position="static" color="inherit">
-            <Typography variant="h2" align="center">Memories</Typography>
-            <img src={memories} alt="memories" height="60" />
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+            <img className={classes.image} src={memories} alt="memories" height="60" />
         </AppBar>
         <Grow in>
             <Container>
