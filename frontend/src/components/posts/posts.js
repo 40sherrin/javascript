a11aa5cdcd1298c0ace0707e823ad3da470a3e4c
40sherrin <fareedaskari42@gmail.com>
2021-03-21 13:56:13 +1100

    Props drilling

--- frontend/src/components/posts/posts.js
+++ frontend/src/components/posts/posts.js
@@ -4,7 +4,7 @@ import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './post/post';
 import useStyles from './styles';
 
-const Posts = () => {
+const Posts = ({setCurrendId}) => {
   const posts = useSelector((state) => state.posts)
   const classes = useStyles(); 
   console.log(posts);
@@ -14,7 +14,7 @@ const Posts = () => {
           {
             posts.map((post) => (
               <Grid key={post._id} item xs={12} sm={6}>
-                  <Post post={post} />
+                  <Post post={post} setCurrendId={setCurrendId} />
               </Grid>
             ))
           }
