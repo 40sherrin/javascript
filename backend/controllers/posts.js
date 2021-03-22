82a3ed3e8e8f2c375a1aaf2844314d8ca40ff16d
40sherrin <fareedaskari42@gmail.com>
2021-03-22 17:02:44 +1100

    Error handling

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -24,7 +24,11 @@ export const createdPost = async (req, res) => {
 export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
-  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
-  res.json(updatedPost)
+  try{
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+    res.json(updatedPost)
+  }catch(err){
+    console.log(err.message);
+  }ss
 }
